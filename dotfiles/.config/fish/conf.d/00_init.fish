7867e6d4fd3588b47d971051058f282e37cc6ad1
furudbat <hircreacc@gmail.com>
2026-06-10 15:40:48 +0200

    fix: cleanup fish

--- dotfiles/.config/fish/conf.d/00_init.fish
+++ dotfiles/.config/fish/conf.d/00_init.fish
@@ -2,13 +2,14 @@
 # INIT
 # -----------------------------------------------------
 
-set -U fish_greeting ""
+# Commands to run in interactive sessions can go here
+if status is-interactive
+    # No greeting
+    set fish_greeting
+
+end
 
 # -----------------------------------------------------
 # Exports
 # -----------------------------------------------------
 export EDITOR=nvim
-
-set -U fish_user_paths /usr/lib/ccache/bin/
-set -U fish_user_paths $fish_user_paths $HOME/.cargo/bin/
-set -U fish_user_paths $fish_user_paths $HOME/.local/bin/
--- dotfiles/.config/fish/conf.d/user_paths.fish
+++ dotfiles/.config/fish/conf.d/user_paths.fish
@@ -1,2 +1,6 @@
 # This is portable across machines/users
-set -gx PATH /usr/lib/ccache/bin $HOME/.cargo/bin $HOME/.local/bin $PATH
+set -gx PATH \
+    /usr/lib/ccache/bin \
+    $HOME/.cargo/bin \
+    $HOME/.local/bin \
+    $PATH
