ef10796fb9527a211541ef698ccbbdd084c18d53
furudbat <hircreacc@gmail.com>
2026-06-10 15:18:47 +0200

    wip: fix timing issues

--- dotfiles/.config/hypr/custom.lua
+++ dotfiles/.config/hypr/custom.lua
@@ -1,6 +1,6 @@
 hl.on("hyprland.start", function () 
     hl.exec_cmd("sleep 5 && otd-daemon")
-    hl.exec_cmd("sleep 3 && ~/.config/bongocat/launch.sh")
+    hl.exec_cmd("sleep 10 && ~/.config/bongocat/launch.sh")
 
     hl.exec_cmd("sleep 5 && ~/.config/hypr/scripts/power-daemon.sh")
     hl.exec_cmd("sleep 10 && ~/.config/hypr/scripts/smart-monitor-power.sh")
