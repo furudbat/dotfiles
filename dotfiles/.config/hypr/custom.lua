ab1d923b6fc3c6df3199e536eaa7e9f75680bfc6
furudbat <hircreacc@gmail.com>
2026-07-04 15:23:26 +0200

    fix: KDE Dolphin open with dialog

--- dotfiles/.config/hypr/custom.lua
+++ dotfiles/.config/hypr/custom.lua
@@ -1,3 +1,8 @@
+-- hl.env("XDG_MENU_PREFIX", "plasma-")
+hl.on("hyprland.start", function () 
+    hl.exec_cmd("sh -lc 'sleep 2 && kbuildsycoca6 --noincremental'")
+end)
+
 hl.on("hyprland.start", function () 
     hl.exec_cmd("sleep 5 && otd-daemon")
     hl.exec_cmd("sleep 10 && ~/.config/bongocat/launch.sh")
